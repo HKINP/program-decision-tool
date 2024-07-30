ed76bfe89a37fb791d9f996a6d3249c81de530de
Ekendra <2187345+ekendra@users.noreply.github.com>
2024-07-30 10:31:56 +0545

    Update app.php

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Decision Support Tool for Cocreation Activities - USAID Integrated Nutrition'),
 
     /*
     |--------------------------------------------------------------------------
