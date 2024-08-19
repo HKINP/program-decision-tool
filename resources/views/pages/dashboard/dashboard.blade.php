afaf80770be64ee9c39ddc1e681e53303f89e4b2
Arun Poudel <arun.poudel48@gmail.com>
2024-08-19 23:04:07 +0545

    dashboard-change

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -10,12 +10,7 @@
                     </div>
                     <div class="mt-4">
                         <h2 class="text-xl font-bold  text-sm">Background</h2>
-                        <p class="text-gray-600 mt-2">USAID Integrated Nutrition is a five-year award (2024-2029) that
-                            supports the Government of Nepal's Multi-Sectoral Nutrition Plan III and the Ministry of
-                            Health and Population's priorities. The objective of USAID Integrated Nutrition is to
-                            improve the nutritional status of children, adolescent girls, and women in Nepal through a
-                            multi-sectoral approach, while prioritizing underserved populations. The award has the
-                            following intermediate results (IR) or intermediate objectives:
+                        <p class="text-gray-600 mt-2">USAID Integrated Nutrition is a five-year award (2024-2029) that supports the Government of Nepal's Multi-Sectoral Nutrition Plan III and the Ministry of Health and Population's priorities. The objective of USAID Integrated Nutrition is to improve the nutritional status of children, adolescent girls, and women in Nepal through a multi-sectoral approach, while prioritizing underserved populations. The award has the following intermediate results (IR) or intermediate objectives:
                         </p>
                     </div>
                     <div class="mt-8">
@@ -24,46 +19,45 @@
                             <li class="text-gray-600">IR 1. Improve household nutrition practices.</li>
                             <li class="text-gray-600">IR 2. Improve quality and coverage of nutrition services</li>
                             <li class="text-gray-600">IR 3. Improve access to safe, diverse, and nutritious foods</li>
+                            <li class="text-gray-600">IR 4. Strengthen GON capacity for multi-sectoral nutrition programming</li>
                         </ul>
                     </div>
                     <div class="mt-8">
                         <h2 class="text-xl font-bold  text-sm">Purpose</h2>
-                        <p class="text-gray-600 mt-2">The purpose of this District Program Design Decision Tool is to
-                            assist multi-sectoral district-level stakeholders in health (including nutrition, family
-                            planning), education, social protection, agriculture, and water, sanitation, and hygiene in
-                            designing activities to address undernutrition in their district. </p>
+                        <p class="text-gray-600 mt-2">The purpose of this District Multi-Sectoral Nutrition Program Design Decision Tool is to assist multi-sectoral district-level stakeholders in health (including nutrition, family planning), education, social protection, agriculture, and water, sanitation, and hygiene in designing activities to address undernutrition in their district. </p>
                     </div>
                     <div class="mt-8">
                         <h2 class="text-xl font-bold  text-sm">Approach</h2>
-                        <p class="text-gray-600 mt-2">Through facilitated discussions, stakeholders will review
-                            existing
-                            data, prioritize behaviors, map platforms and channels, examine barriers, and design
-                            activities that meet the needs of their district. The tool provides a logical step-by-step
-                            process and is divided into 9 steps. The tool encourages stakeholders to use data in their
-                            decision-making process. The district profile sheet accompanies this tool. Since
-                            district-level data are not always available, we recommend stakeholders use their best
-                            judgment. Given USAID Integrated Nutrition's emphasis on addressing the needs of underserved
-                            populations, each step of the tool guides stakeholders to consider data and activities for
-                            everyone in their district and for underserved populations.
-                        </p>
+                        <p class="text-gray-600 mt-2">Through facilitated discussions, stakeholders will review existing data; prioritize behaviors; discuss platforms, channels, and services; examine barriers; and design activities that meet the needs of their district. The tool provides a logical step-by-step process and is divided into 7 steps. The tool encourages stakeholders to use data in their decision-making process. The district profile sheet accompanies this tool. Since district-level data are not always available, we recommend stakeholders use their best judgment. Given USAID Integrated Nutrition's emphasis on addressing the needs of underserved populations, each step of the tool guides stakeholders to consider data and activities for everyone in their district and for underserved populations.</p>
                         <ul class="list-disc pl-4 mt-2">
-                            <li class="text-gray-600">Step 1. Prioritize Behaviors</li>
-                            <li class="text-gray-600">Step 2. Map Platforms, Actors and Channels</li>
-                            <li class="text-gray-600">Step 3 Plan SRC Activities (IR 1)</li>
-                            <li class="text-gray-600">Step 4. Map Health and Nutrition Services</li>
-                            <li class="text-gray-600">Step 5. Pian Health System Activities (IR 2)</li>
-                            <li class="text-gray-600">Step 6. Identify Food System Needs</li>
-                            <li class="text-gray-600">Step 7. Plan Food Systems Activities (IR 3)</li>
-                            <li class="text-gray-600">Step 8. Plan Governance Activities (IR 4)</li>
-                            <li class="text-gray-600">Step 9. Compiled Workplan</li>
-                    </div><p>
-                        This web-based software application - Cocreation Decision Support System is developed as a part of cocreation activities for USAID Integrated Nutrition.
-                        
-                        This application is "made possible by the generous support of the American People through the United States Agency for International Development (USAID)”.
-                        
-                        The information provided on this portal is not official U.S. Government information and does not represent the views or positions of the U.S. Agency for International Development or the U.S. Government.
-                        
-                        Kindly refer to the terms of agreement before using this application.</p>
+                            <li class="text-gray-600">Step 1. District Context</li>
+                            <li class="text-gray-600">Step 2. Prioritize Indicators for Year 1</li>
+                            <li class="text-gray-600">Step 3. SBC Activities</li>
+                            <li class="text-gray-600">Step 4. Health and Nutrition Service Activities</li>
+                            <li class="text-gray-600">Step 5. Food Systems Activities</li>
+                            <li class="text-gray-600">Step 6. Enabling Environment Activities</li>
+                            <li class="text-gray-600">Step 7. Compiled Workplan</li>
+                        </ul>
+                    </div>
+                    <div class="mt-8">
+                        <h2 class="text-xl font-bold  text-sm">Resources referenced</h2>
+                        <ul class="list-disc pl-4 mt-2">
+                            <li class="text-gray-600">Core Group Nutrition Working Group, FANTA III, and Save the Children. 2015. Nutrition Program Design Assistant: A Tool for Program Planners. Version 2. Washington, DC: FANTA/FHI360</li>
+                            <li class="text-gray-600">SPRING. 2016. District Assessment Tool for Anemia: Conducting the District Workshop: Facilitator’s Guide. Arlington, VA: SPRING</li>
+                            <li class="text-gray-600">USAID Advancing Nutrition. 2021. Using Research to Design A Social and Behavior Change Strategy for Multi-Sectoral Nutrition. Arlington, VA: USAID Advancing Nutrition</li>
+                        </ul>
+                    </div>
+                    <div class="mt-8">
+                        <p class="italic">
+                            This web-based software application - Cocreation Decision Support System is developed as a part of cocreation activities for USAID Integrated Nutrition.
+                            
+                            <br>This application is "made possible by the generous support of the American People through the United States Agency for International Development (USAID)”.
+                            
+                            <br>The information provided on this portal is not official U.S. Government information and does not represent the views or positions of the U.S. Agency for International Development or the U.S. Government.
+                            
+                            <br><br>Kindly refer to the terms of agreement before using this application.
+                        </p>
+                    </div>
                 </div>
 
             </div>
