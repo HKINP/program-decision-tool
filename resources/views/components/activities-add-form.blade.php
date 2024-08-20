391fb75bb3e466daa3cb7bee2e5a1bd80314f94c
Pravin Poudel <pravin.2047@gmail.com>
2024-08-20 22:34:25 +0545

    minor changes

--- resources/views/components/activities-add-form.blade.php
+++ resources/views/components/activities-add-form.blade.php
@@ -22,17 +22,18 @@
                                 class="font-normal">{{ $question->thematicArea->thematic_area }}</span></p>
                         <input type="number" name="thematic_area_id" value="{{ $question->thematic_area_id }}" hidden>
                     </div>
-                  
-                    <div class="mb-4 ">
-                        <p class="font-semibold text-md text-black">{{ $index + 1 }}.1 Key Barriers @if($stageId != 5) * @endif</p>
-                        <textarea id="key_barriers" name="key_barriers" rows="4"
-                            
-                        @if($stageId != 5) required @endif
 
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    <div class="mb-4 ">
+                        <p class="font-semibold text-md text-black">{{ $index + 1 }}.1 Key Barriers @if ($stageId != 5)
+                                *
+                            @endif
+                        </p>
+                        <textarea id="key_barriers" name="key_barriers" rows="4" @if ($stageId != 5) required @endif
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Write your notes here...">{{ $keybarriers->has($question->question_id) ? $keybarriers->get($question->question_id)->first()->key_barriers : '' }}</textarea>
                         @if ($keybarriers->has($question->question_id))
-                        <input type="number" name="key_barriers_id" value="{{ $keybarriers->get($question->question_id)->first()->id }}" hidden>
+                            <input type="number" name="key_barriers_id"
+                                value="{{ $keybarriers->get($question->question_id)->first()->id }}" hidden>
                         @endif
                     </div>
                     <p class="font-semibold text-md text-black mb-4">{{ $index + 1 }}.2 Sub Activities</p>
@@ -50,10 +51,9 @@
                             </tr>
                         </thead>
                         <tbody>
-                           
+
                             @if (isset($activities[$question->question_id]))
                                 @foreach ($activities[$question->question_id] as $activity)
-                             
                                     <tr>
                                         <td class="border border-gray-300 p-2 text-sm">
                                             {{ $activity->proposed_activities }}
@@ -62,22 +62,23 @@
                                             {{ $activity->targeted_for }}
                                         </td>
                                         <td class="border border-gray-300 p-2 text-sm">
-                                            @foreach($activity->platforms as $platform)
-                                            <li class="">
-                                                {{ $platform->platforms }}
-                                            </li>
-                                        @endforeach
+                                            @foreach ($activity->platforms as $platform)
+                                                <li class="">
+                                                    {{ $platform->platforms }}
+                                                </li>
+                                            @endforeach
                                         </td>
                                         <td class="border border-gray-300 p-2 text-sm">
                                             {{ $activity->remarks }}
                                         </td>
                                         <td class="border border-gray-300 p-2 text-sm">
                                             <div class="flex space-x-4">
-                                              
+
                                                 <!-- <a href="{{ route('activities.edit', $activity->id) }}" class="text-yellow-500 hover:text-yellow-700">
                                                     <i class="fas fa-edit"></i>
                                                 </a> -->
-                                                <button type="button" class="text-red-500 hover:text-red-700" onclick="showDeleteModal('{{ route('prioritizedActivities.destroy', $activity->id) }}')">
+                                                <button type="button" class="text-red-500 hover:text-red-700"
+                                                    onclick="showDeleteModal('{{ route('prioritizedActivities.destroy', $activity->id) }}')">
                                                     <i class="fas fa-trash"></i>
                                                 </button>
                                             </div>
@@ -91,19 +92,34 @@
                     </table>
 
 
-                    
+
                     <div id="activities-container-{{ $question->question_id }}" class="space-y-4">
 
+                    </div>
+                    <div class="flex items-center justify-between mt-4">
+                        <!-- Left side: Text -->
+                        <p class="italic text-sm hidden font-semibold" id="notessubmit">Submit or update before adding a new
+                            activities !!</p>
 
+                        <!-- Right side: Buttons -->
+                        <div class="flex items-center gap-4">
+                            <div class="text-right">
+                                <button type="submit" class="p-2 bg-green-500 text-white rounded">
+                                    @if ($keybarriers->has($question->question_id))
+                                        Update
+                                    @else
+                                        Submit
+                                    @endif
+                                </button>
+                            </div>
+
+                            <button type="button" class="p-2 bg-blue-500 text-white rounded flex items-center gap-2"
+                                onclick="addActivity({{ $question->question_id }})">
+                                Add +
+                            </button>
+                        </div>
                     </div>
-                    <button type="button" class="mt-2 bg-blue-500 text-white p-2 rounded flex items-center gap-2"
-                        onclick="addActivity({{ $question->question_id }})">
-                        Add Activities
-                    </button>
-                    <div class="text-right">
-                       
-                        <button type="submit" class="mt-4 p-2 bg-green-500 text-white  rounded"> @if ($keybarriers->has($question->question_id)) Update @else Submit @endif</button>
-                    </div>
+
                 </div>
 
             </form>
@@ -114,78 +130,93 @@
         <input type="number" name="province_id" value="{{ $districtProfile->province->id }}" hidden>
         <input type="number" name="district_id" value="{{ $districtProfile->id }}" hidden>
         <input type="number" name="stage_id" value="{{ $stageId }}" hidden>
-    <div class="bg-white p-4 rounded-lg w-full mb-5">
-        <div class="flex gap-2 items-center mb-4">
-            <p class="h-10 w-10 bg-[#F1F3F8] rounded-full flex items-center justify-center font-semibold">06</p>
-            <p class="font-semibold text-md text-blue-600">Notes</p>
-        </div>
-        <div class="space-y-2 text-xs italic">
-            <textarea id="notes" name="notes" rows="4" required
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Write your notes here...">{{ $stepRemarks->notes ?? ""}}</textarea>
-        </div>
-        <div class="text-right">
-                       
-            <button type="submit" class="mt-4 p-2 bg-purple-900 text-white  rounded">Save Notes and Next  </button>
-        </div>
+        <div class="bg-white p-4 rounded-lg w-full mb-5">
+            <div class="flex gap-2 items-center mb-4">
+                <p class="h-10 w-10 bg-[#F1F3F8] rounded-full flex items-center justify-center font-semibold">06</p>
+                <p class="font-semibold text-md text-blue-600">Notes *</p>
+            </div>
+            <div class="space-y-2 text-xs italic">
+                <textarea id="notes" name="notes" rows="4" required
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Write your notes here...">{{ $stepRemarks->notes ?? '' }}</textarea>
+            </div>
+            <div class="text-right">
+
+                <button type="submit" class="mt-4 p-2 bg-purple-900 text-white  rounded">Complete this step <i
+                        class="text-white fa-md fa-solid fa-arrow-right"></i></button>
+            </div>
     </form>
     </div>
-<!-- Delete Modal -->
-<div id="delete-modal" class="hidden fixed z-10 inset-0 overflow-y-auto">
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div class="fixed inset-0 transition-opacity">
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-lg sm:w-full">
-            <form id="delete-form" method="POST">
-                @csrf
-                @method('DELETE')
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div class="sm:flex sm:items-start">
-                        <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-                            </svg>
-                        </div>
-                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                Delete Sub Activities
-                            <div class="mt-2">
-                                <p class="text-sm leading-5 text-gray-500">
-                                    Are you sure you want to delete this sub activity? This action cannot be undone.
-                                </p>
+    <!-- Delete Modal -->
+    <div id="delete-modal" class="hidden fixed z-10 inset-0 overflow-y-auto">
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 transition-opacity">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
+            <div
+                class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-lg sm:w-full">
+                <form id="delete-form" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div class="sm:flex sm:items-start">
+                            <div
+                                class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </div>
+                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                    Delete Sub Activities
+                                    <div class="mt-2">
+                                        <p class="text-sm leading-5 text-gray-500">
+                                            Are you sure you want to delete this sub activity? This action cannot be
+                                            undone.
+                                        </p>
+                                    </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-                        <button type="submit" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                            Delete
-                        </button>
-                    </span>
-                    <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-                        <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5" onclick="document.getElementById('delete-modal').classList.add('hidden')">
-                            Cancel
-                        </button>
-                    </span>
-                </div>
-            </form>
+                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                            <button type="submit"
+                                class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Delete
+                            </button>
+                        </span>
+                        <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                            <button type="button"
+                                class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                                onclick="document.getElementById('delete-modal').classList.add('hidden')">
+                                Cancel
+                            </button>
+                        </span>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-</div>
+    </div>
 
 
     <script>
         function showDeleteModal(deleteRoute) {
-        document.getElementById('delete-form').action = deleteRoute;
-        document.getElementById('delete-modal').classList.remove('hidden');
-    }
+            document.getElementById('delete-form').action = deleteRoute;
+            document.getElementById('delete-modal').classList.remove('hidden');
+        }
+
         function addActivity(questionId) {
             const container = document.getElementById(`activities-container-${questionId}`);
             const activityCount = container.children.length + 1;
+            if (activityCount > 0) {
+                document.querySelector('#notessubmit').classList.remove('hidden');
+            } else {
+                document.querySelector('#notessubmit').classList.add('hidden');
+            }
 
             const platformsOptions = @json(
                 $platforms->map(function ($platform) {
@@ -242,12 +273,10 @@
             </div>
         `;
 
-            $(container.appendChild(activityDiv)).find('.multipleselect').select2(
-                {
+            $(container.appendChild(activityDiv)).find('.multipleselect').select2({
                 placeholder: 'Select Platforms',
                 allowClear: true
-            }
-            );
+            });
 
 
         }
